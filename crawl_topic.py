ced7f5038f76bde0d38c326f9195def486333104
Hung Le Viet <hunglv@tech.est-rouge.com>
2023-11-10 13:53:47 +0700

    add encoding japanese when saving file

--- crawl_topic.py
+++ crawl_topic.py
@@ -14,25 +14,25 @@ def crawl_data(input_file, output_file):
     data2 = []
     for li in li_elements:
         for item in li.find_all('img'):
-            lesson_name = item['alt']
+            topic_name = item['alt']
             image = item['src']
-            data1.append({'lesson_name': lesson_name, 'image': image})
+            data1.append({'topic_name': topic_name, 'image': image})
 
 
     for li in li_elements:
         for item in li.find_all("a"):
             if "content" in item['href']:
-                lesson_name = item.text
+                topic_name = item.text
                 url = "https://www.eboard.jp" + item['href']
-                data2.append({'lesson_name': lesson_name, 'url': url})
+                data2.append({'topic_name': topic_name, 'url': url})
 
     df1 = pd.DataFrame(data1)
     df2 = pd.DataFrame(data2)
 
-    merged_df  = pd.merge(df1,df2, on = "lesson_name", how='left')
-    merged_df.to_csv(output_file, index=False)
+    merged_df  = pd.merge(df1,df2, on = "topic_name", how='left')
+    merged_df.to_csv(output_file, index=False, encoding='utf_8_sig')
 
 if __name__ == "__main__":
-    input_file = "data/secondary_school/eboard_society_topic.html"
-    output_file = "data/secondary_school/eboard_society_topic.csv"
+    input_file = "data/primary_school/eboard_arithmetic.html"
+    output_file = "data/primary_school/eboard_arithmetic.csv"
     crawl_data(input_file,output_file)
